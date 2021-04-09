c137c0ca57d28ef9a9d3e5030e2943ae278f8ab3
Logesh R <rlogeshk@ford.com>
2021-04-09 11:51:17 +0530

    Created OKR retrive service and added unit tests for controller

--- src/main/java/okr/retrive/ObjectiveKeyResult.java
+++ src/main/java/okr/retrive/ObjectiveKeyResult.java
@@ -1,44 +1,16 @@
 package okr.retrive;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Builder
+@Getter
 public class ObjectiveKeyResult {
 	
 	String description;
 	Integer parentId;
 	Integer id;
-	Double progress;
-	
-	
-	
-	public ObjectiveKeyResult(String description, Integer parentId, Integer id, Double progress) {
-		super();
-		this.description = description;
-		this.parentId = parentId;
-		this.id = id;
-		this.progress = progress;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public Integer getParentId() {
-		return parentId;
-	}
-	public void setParentId(Integer parentId) {
-		this.parentId = parentId;
-	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public Double getProgress() {
-		return progress;
-	}
-	public void setProgress(Double progress) {
-		this.progress = progress;
-	}
-	
+	Double progress;	
 }
