c1646d3a8d9ad71af3774092c714af1958adfcdc
Aishwarya Kalamegam, Aparna Venkata Oruganti and Logesh R <pair+kaishwa2+oaparnav+rlogeshk@ford.com>
2021-04-16 12:43:45 +0530

    Added MVC test for controller

---
+++ src/test/java/okr/retrive/ControllerMvcTest.java
@@ -0,0 +1,22 @@
+package okr.retrive;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+public class ControllerMvcTest {
+	
+	@Autowired
+	MockMvc mockMvc;
+
+	@Test
+	public void controllerReturnsHelloText() throws Exception {
+		
+		mockMvc.perform(get("/getObjective/10")).andExpect(status().isOk());
+	}
+}
